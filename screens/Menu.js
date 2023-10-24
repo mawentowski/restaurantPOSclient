d13aca812e2198fbecdb8fcc21be5d85bd97ee90
mawentowski <mawentowskigit@gmail.com>
2023-10-24 22:47:29 +0700

    tabs works ;

--- screens/Menu.js
+++ screens/Menu.js
@@ -1,14 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, Text, Button } from "react-native";
-import MenuTabs from "./MenuTabs";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import AppetizerScreen from "./Appetizer";
+import AppetizerScreen from "./Appetizer"; // Import the AppetizerScreen component
+import MainCourseScreen from "./MainCourse"; // Import the MainCourseScreen component
+import DessertScreen from "./Dessert"; // Import the MainCourseScreen component
 
+// Define your tab screens
 const Tab = createMaterialTopTabNavigator();
 
-const Menu = () => {
-  const [selectedItems, setSelectedItems] = useState([]);
+const MenuTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Appetizer" component={AppetizerScreen} />
+      <Tab.Screen name="MainCourse" component={MainCourseScreen} />
+      <Tab.Screen name="Dessert" component={DessertScreen} />
+    </Tab.Navigator>
+  );
+};
 
+const App = () => {
   const confirmOrder = () => {
     // Implement your confirm order logic
   };
@@ -22,10 +32,24 @@ const Menu = () => {
   };
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Appetizer" component={AppetizerScreen} />
-    </Tab.Navigator>
+    <>
+      {/* Your app header or navigation can go here */}
+      <View style={{ flex: 1 }}>
+        <MenuTabs />
+        <View
+          style={{
+            backgroundColor: "red",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <Button title="Confirm order" onPress={confirmOrder} />
+          <Button title="Call waiter" onPress={callWaiter} />
+          <Button title="View bill" onPress={viewBill} />
+        </View>
+      </View>
+    </>
   );
 };
 
-export default Menu;
+export default App;
