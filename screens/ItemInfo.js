2d5211704d877c29e4bb4913727dbf9b8583606d
mawentowski <mawentowskigit@gmail.com>
2023-11-11 11:54:01 +0700

    added header

--- screens/ItemInfo.js
+++ screens/ItemInfo.js
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, ScrollView, Image } from "react-native";
 import { Appbar, FAB, useTheme, Text, Button } from "react-native-paper";
 import CounterComponent from "../components/Counter";
 import { CartItemsContext } from "../store/context/CartItemsContext"; // Import your CartItemsContext
 import * as calculateCostUtils from "../utils/calculateCost";
 
 const ItemInfoScreen = ({ route, navigation }) => {
+  const theme = useTheme();
   const { item } = route.params;
   const cartItemsCtx = useContext(CartItemsContext);
 
@@ -44,56 +45,65 @@ const ItemInfoScreen = ({ route, navigation }) => {
   const roundedSingleItemPrice = calculateCostUtils.roundCost(item.price);
 
   return (
-    <View>
-      <Text>
-        {item.id}, {item.name}, {item.image}, {roundedSingleItemPrice}. The
-        count is: {count}
-      </Text>
-      <CounterComponent
-        count={count}
-        increment={increment}
-        decrement={decrement}
-        price={roundedSingleItemPrice}
-      />
-      <Button
-        mode="contained"
-        // title={
-        //   count === 0
-        //     ? "Back to Menu"
-        //     : `Add to order - $${calculateCostUtils.calculateCostByCount(
-        //         count,
-        //         roundedSingleItemPrice
-        //       )}`
-        // }
-        onPress={() => {
-          if (count > 0 && count !== currentQuantity) {
-            // If the count is different, update the cart with the new quantity
-            cartItemsCtx.setCartItemQuantity(item.id, count);
-            console.log("Quantity updated");
-          }
-          if (count === 0 && currentQuantity !== 0) {
-            cartItemsCtx.removeCartItem(item.id);
-          }
+    <>
+      <Appbar.Header style={{ backgroundColor: theme.colors.elevation.level2 }}>
+        <Appbar.Content title={<CartItemHeroImage item={item} />} />
+      </Appbar.Header>
+      <View>
+        <Text>
+          {item.id}, {item.name}, {item.image}, {roundedSingleItemPrice}. The
+          count is: {count}
+        </Text>
+        <CounterComponent
+          count={count}
+          increment={increment}
+          decrement={decrement}
+          price={roundedSingleItemPrice}
+        />
+        <Button
+          mode="contained"
+          // title={
+          //   count === 0
+          //     ? "Back to Menu"
+          //     : `Add to order - $${calculateCostUtils.calculateCostByCount(
+          //         count,
+          //         roundedSingleItemPrice
+          //       )}`
+          // }
+          onPress={() => {
+            if (count > 0 && count !== currentQuantity) {
+              // If the count is different, update the cart with the new quantity
+              cartItemsCtx.setCartItemQuantity(item.id, count);
+              console.log("Quantity updated");
+            }
+            if (count === 0 && currentQuantity !== 0) {
+              cartItemsCtx.removeCartItem(item.id);
+            }
 
-          navigation.navigate("Menu");
-        }}
-        // cant get styling to work
-        style={[
-          styles.button,
-          count !== 0 && styles.greenButton,
-          count === 0 && currentQuantity !== 0 && styles.redButton,
-          count === 0 && currentQuantity === 0 && styles.blueButton,
-        ]}
-      >
-        {count === 0
-          ? "Back to Menu"
-          : `Add to order - $${calculateCostUtils.calculateCostByCount(
-              count,
-              roundedSingleItemPrice
-            )}`}
-      </Button>
-    </View>
+            navigation.navigate("Menu");
+          }}
+          // cant get styling to work
+          style={[
+            styles.button,
+            count !== 0 && styles.greenButton,
+            count === 0 && currentQuantity !== 0 && styles.redButton,
+            count === 0 && currentQuantity === 0 && styles.blueButton,
+          ]}
+        >
+          {count === 0
+            ? "Back to Menu"
+            : `Add to order - $${calculateCostUtils.calculateCostByCount(
+                count,
+                roundedSingleItemPrice
+              )}`}
+        </Button>
+      </View>
+    </>
   );
+};
+
+const CartItemHeroImage = (item) => {
+  return <Image style={styles.itemImage} source={item.image} />;
 };
 
 const styles = StyleSheet.create({
@@ -122,6 +132,16 @@ const styles = StyleSheet.create({
 
   redButton: {
     color: "red",
+  },
+
+  itemImage: {
+    // padding: 8,
+    // marginLeft: "7.5%",
+    // marginRight: "7.5%",
+    // width: "85%",
+    // width: "85%",
+    aspectRatio: 1,
+    // borderRadius: 18,
   },
 });
 
